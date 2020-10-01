fddd34e964d4cfd753216cbd48abb77f1a334074
mannymodrono <mmodrono21243@columbushs.com>
2020-10-01 10:59:41 -0400

    Started

---
+++ Car.java
@@ -0,0 +1,13 @@
+public class Car {
+    //instance var
+    private double startMiles;
+    private double endMiles;
+    private double gallons;
+
+    //Constructor
+    public Car (double pStartMiles, double pEndMiles, double pGallons) {
+        this.startMiles = pStartMiles;
+        this.endMiles = pEndMiles;
+        this.gallons = pGallons;
+    }
+}
