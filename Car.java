d3c42ac7d20820b6fa8b2c9d8738f06c5cc2e07c
mannymodrono <mmodrono21243@columbushs.com>
2020-10-05 09:57:59 -0400

    finished

--- Car.java
+++ Car.java
@@ -10,4 +10,23 @@ public class Car {
         this.endMiles = pEndMiles;
         this.gallons = pGallons;
     }
+
+    //methods
+
+    public void setGallons(double pGallons) {
+        this.gallons = pGallons;
+    }
+
+    public double getGallons() {
+        return this.gallons;
+    }
+
+    public double getMPG() {
+        double totalMiles = this.endMiles - this.startMiles;
+        return totalMiles/this.gallons;
+    }
+
+    public String toString(){
+        return "Starting miles: " + this.startMiles + "\nEnding miles: " + this.endMiles + "\nGallons used: " + this.gallons + "\n\nMPG: " + this.getMPG();
+    }
 }
--- CarDemo.java
+++ CarDemo.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class CarDemo {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Starting miles?");
+        double startMiles = input.nextDouble();
+
+        System.out.println("Ending miles?");
+        double endMiles = input.nextDouble();
+
+        System.out.println("Gallons used?");
+        double gallons = input.nextDouble();
+
+        input.close();
+
+        Car miata = new Car(startMiles, endMiles, gallons);
+
+        System.out.println("\n\n" + miata.toString());
         
     }
 }
